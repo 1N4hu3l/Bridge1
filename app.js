3d7c18d405fea14b52e217af4e4a606acfbf8b23
Nahuel.H <nahuel.a.hermosilla@gmail.com>
2024-08-19 17:00:26 -0300

    a

--- app.js
+++ app.js
@@ -156,11 +156,15 @@ app.post('/register', async (req, res) => {
     
     
 
-app.get('/logout', (req,res)=>{
-    req.session.destroy(()=>{
-        res.redirect('/')
-    })
-})
+    app.post('/logout', (req, res) => {
+        req.session.destroy(err => {
+            if (err) {
+                return res.redirect('/');
+            }
+            res.clearCookie('connect.sid');
+            res.redirect('/login');
+        });
+    });
 
 
 ////////////////////
